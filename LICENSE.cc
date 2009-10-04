e607291da3061b2c1493e71bade142dec1f24ad6
sebastien.lelong <sebastien.lelong@832e52a4-704d-11de-9895-81829a6523ab>
2009-10-04 17:51:54 +0000

    please read: Jaluino licences

---
+++ LICENSE.cc
@@ -0,0 +1,8 @@
+Jaluino by Jaluino authors (http://code.google.com/p/jaluino/people/list) 
+is licensed under a Creative Commons Attribution-Share Alike 3.0 Unported License.
+Based on a work at http://code.google.com/p/jaluino/
+Permissions beyond the scope of this license may be available at http://code.google.com/p/jaluino/.
+
+To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to 
+Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+
