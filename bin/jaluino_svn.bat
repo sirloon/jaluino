0997e2f59efa7a9384d5950b7ff7fc4cd518b535
sebastien.lelong <sebastien.lelong@832e52a4-704d-11de-9895-81829a6523ab>
2010-02-17 16:58:20 +0000

    dos wrapper to compile jaluino/jallib samples from SVN

---
+++ bin/jaluino_svn.bat
@@ -0,0 +1,43 @@
+@ echo off
+rem  Title: jallib executable for windows system
+rem  Author: Sebastien Lelong, Copyright (c) 2008, all rights reserved.
+rem  Adapted-by: Joep Suijs
+rem  Compiler:
+rem
+rem  This file is part of jallib (http://jallib.googlecode.com)
+rem  Released under the BSD license (http://www.opensource.org/licenses/bsd-license.php)
+rem
+rem  Sources:
+rem
+rem  Description: the aim of this script is to ease user's life !
+rem  If *you* are a user, edit this script by specfiying the following
+rem  environment variables. These variables will save you some typing
+rem  while using the jallib wrapper script. Adapt it to your own need !
+rem  Once done, you can put this script in your path.
+rem  Rem: usually, setting JALLIB_ROOT as an absolute path is enough.
+
+
+rem ########################
+rem ENVIRONMENT VARIABLES #
+rem ######################
+
+
+rem call from something above, or directly called ?
+set RUNFROM=%CD%\..
+if defined JALUINO_ROOT set RUNFROM=%JALUINO_ROOT%
+
+rem Path to the jallib root's repository (containing "tools,"compiler","include" dirs)
+set JALLIB_ROOT=%RUNFROM%\3rdparty\jallib_svn
+set JALUINO_ROOT=%RUNFROM%
+rem path to jallib root's libraries.
+set JALLIB_REPOS=%JALUINO_ROOT%\lib;%JALLIB_ROOT%\include
+rem path to jallib root's samples.
+set JALLIB_SAMPLEDIR=%JALUINO_ROOT%\samples
+rem path to "jalv2" executable. If not in your PATH, set an absolute path to the exec
+rem Arguments can be added if they're needed as default (Ex: "jalv2 -long-start")
+set JALLIB_JALV2=%JALLIB_ROOT%\compiler\jalv2.exe
+
+
+set JALLIB_PYTHON=%PYTHON_EXEC%
+%JALLIB_PYTHON% "%JALLIB_ROOT%\tools\jallib.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
