2a5fc1454238bc51b882c36b8ad7d50fb0e6aec0
sebastien.lelong <sebastien.lelong@832e52a4-704d-11de-9895-81829a6523ab>
2010-02-22 17:24:45 +0000

    fix spaces in commandline

--- bin/tinybl.bat
+++ bin/tinybl.bat
@@ -3,5 +3,5 @@
 rem wrapper around tinybld2 to call it with absolute path
 rem so tinybld2 can find its configuration file
 
-%JALUINO_BIN%\tinybld2w.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%JALUINO_BIN%\tinybld2w.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
 
