c6a41815753a1db3a48345f18df5bde5663f2634
carlo.dormeletti@email.it <carlo.dormeletti@email.it@832e52a4-704d-11de-9895-81829a6523ab>
2010-12-02 20:37:28 +0000

    alpha release

---
+++ ide/plugins/jpk2cmd/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Setup script to build the Jaluino Pk2cmd plugin. To build the plugin
+# just run 'python setup.py bdist_egg' and an egg will be built and put into 
+# the dist directory of this folder.
+
+"""PK2-JaluinoIDE"""
+
+from setuptools import setup
+ 
+__author__ = "Carlo Dormeletti"
+__doc__ = """Plugin to control PicKit2 in the JaluinoIDE"""
+__version__ = "0.0.2"
+ 
+setup(
+      name    = "PK2-JaluinoIDE",    # Plugin Name
+      version = __version__,         # Plugin Version
+      description = __doc__,         # Short plugin description
+      author = __author__,     
+      author_email = "carlo.dormeletti@email.it", 
+      license = "wxWindows",       # Plugins licensing info
+      packages = ['jpk2cmd'],     # Package directory name(s)
+      entry_points = '''
+      [Editra.plugins]
+      JPK2 = jpk2cmd:Jpk2cmd
+      '''
+     )
